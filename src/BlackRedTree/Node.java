db3c8ffa973ef33d60f8f95d6a94a28627e61c0f
aglassoforange <46038773+aglassoforange@users.noreply.github.com>
2025-02-05 22:01:32 -0500

    update node

--- src/BlackRedTree/Node.java
+++ src/BlackRedTree/Node.java
@@ -16,6 +16,23 @@ public class Node <Key extends Comparable<Key>, Value>{
             this.color = color;
     }
 
+    public add(Key key, Value value){
+        if (key.compareTo(this.key) < 0){
+            if (this.left == null){
+                this.left = new Node(key, value, null, null, true);
+            } else {
+                this.left.add(key, value);
+            }
+        } else if (key.compareTo(this.key) > 0){
+            if (this.right == null){
+                this.right = new Node(key, value, null, null, true);
+            } else {
+                this.right.add(key, value);
+            }
+        } else {
+            this.value = value;
+        }
+    }
 
 
 }
